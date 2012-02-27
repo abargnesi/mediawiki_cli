4c3373f0be73839031623a9eebeb5b80608981a1
Anthony Bargnesi <tony@den-4.com>
2012-02-26 19:11:43 -0500

    added new append.py script - allows appending text to a wiki page by supplying the 2nd argument - if 2nd argument is not supplied then a vim buffer is opened dfaddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd

---
+++ append.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+#
+# Append to page command:
+# Inputs:
+#   PAGE_NAME - Name of a wiki page.
+#
+
+import init
+import datetime
+import string
+import subprocess
+import sys
+import tempfile
+
+if len(sys.argv) not in (2,3):
+    print "usage: append [PAGE_NAME] [TEXT_TO_APPEND] (leave off for vim buffer)"
+    sys.exit(1)
+
+append_arg=None
+if len(sys.argv) is 3:
+    append_arg = sys.argv[2]
+
+mw = init.mw("[HOST]", "[PATH]")
+page_name = sys.argv[1]
+log_page = mw.site.Pages[page_name]
+page_text = log_page.edit()
+
+# append text, save and exit if append text provided
+if append_arg is not None:
+    updated_page_text = string.join((page_text,'<br/>',append_arg))
+    log_page.save(updated_page_text, summary='')
+    print "Saved update to %s." % (page_name)
+    sys.exit(0)
+
+# delegate to vim for the append text
+date = datetime.date.today()
+try:
+    temp = tempfile.NamedTemporaryFile(suffix='.logpage',prefix=str(date))
+    vim_cmd = ['vim', str(temp.name)]
+    vim_proc = subprocess.call(vim_cmd)
+    edit = temp.read()
+  
+    if edit in ("",None):
+        print "No update; exit without saving."
+        sys.exit(0)
+
+    updated_page_text = string.join((page_text,'<br/>',edit),'')
+
+    log_page.save(updated_page_text, summary='')
+    print "Saved update to %s." % (page_name)
+finally:
+    temp.close()
+
